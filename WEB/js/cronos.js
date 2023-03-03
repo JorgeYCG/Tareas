9bb5090ee8f366b7ced7822488d97c58968becc4
JorgeYCG <126737839+JorgeYCG@users.noreply.github.com>
2023-03-02 23:12:20 -0600

    Add files via upload

---
+++ WEB/js/cronos.js
@@ -0,0 +1,101 @@
+<!--inicia funciones del cronometro-->
+const stopwatch = document.getElementById('stopwatch');
+const playPauseButton = document.getElementById('play-pause');
+const secondsSphere = document.getElementById('seconds-sphere');
+
+let stopwatchInterval;
+let runningTime = 0;
+
+const playPause = () => {
+    const isPaused = !playPauseButton.classList.contains('running');
+    if (isPaused) {
+        playPauseButton.classList.add('running');
+        start();
+    } else {
+        playPauseButton.classList.remove('running');
+        pause();
+    }
+}
+
+const pause = () => {
+    secondsSphere.style.animationPlayState = 'paused';
+    clearInterval(stopwatchInterval);
+}
+
+const stop = () => {
+    secondsSphere.style.transform = 'rotate(-90deg) translateX(60px)';
+    secondsSphere.style.animation = 'none';
+    playPauseButton.classList.remove('running');
+    runningTime = 0;
+    clearInterval(stopwatchInterval);
+    stopwatch.textContent = '00:00:00.0';
+}
+
+const start = () => {
+    secondsSphere.style.animation = 'rotacion 60s linear infinite';
+    let startTime = Date.now() - runningTime;
+    secondsSphere.style.animationPlayState = 'running';
+    stopwatchInterval = setInterval( () => {
+        runningTime = Date.now() - startTime;
+        stopwatch.textContent = calculateTime(runningTime);
+    }, 1000)
+}
+
+const calculateTime = runningTime => {
+    const total_seconds = Math.floor(runningTime / 1000);
+    const total_minutes = Math.floor(total_seconds / 60);
+    const total_hours=Math.floor(total_minutes / 60);
+    const display_seconds = (total_seconds % 60).toString().padStart(2, "0");
+    const display_minutes = total_minutes.toString().padStart(2, "0");
+    const display_hours = total_hours.toString().padStart(2, "0");
+
+    return `${display_hours}:${display_minutes}:${display_seconds}`
+}
+
+<!--termina funciones del cronometro-->
+
+<!--inicia funciones del caracter-->
+function validar(){
+    var letras = document.getElementById("letra").value;
+    if (letras==''){
+        alert("por favor digita una letra a validar no puede estar el campo vacio. ");
+
+    }
+    else if(letras){
+        alert("nesesitamos que dijites una letra:" );
+    }
+
+    else{
+        switch(letras){
+            case'a';
+            case'e';
+            case'i';
+            case'e';
+            case'o';
+            case'u';
+            alert("la letra que dijitaste es: "+letras+"es una vocal");
+            break;
+            default: 
+            alert("la letra que dijitaste es "+letras+"es una consonante");
+        }
+    }
+}
+<!--termina la funciones del caracter-->
+
+<!--inicia funciones de horas trabajadas-->
+
+        function contenido(id){
+            return document.getElementById(id)
+}
+        function calcularsal(){
+        var pn= parseInt(contenido('sal').value)
+        var porcent=100;
+        const incre=28;
+        const decre=20;
+       if (pn>=28) {
+        (incre * porcent)/pn;
+        else{
+            (decre * porcent)/pn;
+        }
+    }
+}
